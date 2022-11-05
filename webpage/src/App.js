a76dc836f0f5b323c912348e4e0bf79826feb985
Jaden Nguyen <21jadennguyen@gmail.com>
2022-11-05 18:50:51 -0400

    initial webpage

---
+++ webpage/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Topbar from './Topbar/index.js';
+import Album from './Album/index.js';
+
+function App() {
+  return (
+    <div>
+      <Topbar />
+      <Album />
+    </div>
+  );
+}
+
+export default App;
---
+++ webpage/src/Topbar/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+
+function Topbar() {
+    return (
+        <div className = "topbar">
+            <div className="wrapper">
+                <h1> Camp Explorer </h1>
+            </div>   
+        </div>
+    );
+}
+
+export default Topbar;
