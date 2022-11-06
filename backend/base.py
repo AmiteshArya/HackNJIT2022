451f2955d60e0ffedeeb3a59243ec90f90217bb5
quagum <quagum125@gmail.com>
2022-11-06 01:44:49 -0400

    cleaning of file inputs

--- backend/base.py
+++ backend/base.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, flash
 import os 
 from functions import getTags, sort
 
@@ -7,9 +7,7 @@ api = Flask(__name__)
 
 @api.route('/sort')
 def sort_images():
-
     response_body = sort("./images")
-
     return response_body
 
 
@@ -20,9 +18,13 @@ def upload_images():
     file_folder = request.files['file']
     ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
     UPLOAD_FOLDER = "./images"
-
-    for file in file_folder:
-        if file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
-            file.save(os.path.join(api.config['UPLOAD_FOLDER'], filename))
-
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file found") 
+        if file_folder: #from werkzeug.utils import secure_filename --> to prevent .php and html uploads --> hassle in dependecies
+            for file in file_folder:
+                file_path = file.filename
+                if file_path.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+                    file.save(os.path.join(api.config[UPLOAD_FOLDER], file_path))
+            
 
